3746385c238717be774376b63bd3eb7ae7913d3e
exe1x <exefnbr@gmail.com>
2024-11-09 23:09:01 -0800

    yeeeee

--- src/app/components/Galaxy.js
+++ src/app/components/Galaxy.js
@@ -64,7 +64,7 @@ const GalaxyD3 = () => {
                         orbitDataRef.current.push({
                             ...holder,
                             orbitRadius: 50 + Math.random() * (Math.min(width, height) / 2 - 50),
-                            orbitSpeed: 0.00001,
+                            orbitSpeed: 0.00007,
                             size: 1.5 * sizeScale(holder.amount),
                             angle: Math.random() * 2 * Math.PI
                         });
